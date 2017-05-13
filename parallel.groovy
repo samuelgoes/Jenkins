464a4469df2e45721712904d72ae8d646aedaf4f
Samuel Gómez Escalante <samuel.gomez.contractor@bbva.com>
2017-05-13 11:55:16 +0200

    stage deprecated

--- parallel.groovy
+++ parallel.groovy
@@ -9,11 +9,12 @@ node {
     def branches = [:]
     for (int i = 0; i < numHelloMessages.toInteger(); i++) {
       branches["split${i}"] = {
-        stage "Stage parallel- #"+i
-        node('remote') {
-         echo  'Starting sleep'
-         sleep 10
-         echo  'Finished sleep'
+        stage("Stage parallel- #"+i){
+          node('remote') {
+            echo  'Starting sleep'
+            sleep 10
+              echo  'Finished sleep'
+          }
         }
       }
     }
