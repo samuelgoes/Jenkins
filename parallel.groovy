c969f0ebc4bd4b7c0338e9c16dd5c7afeb234494
Samuel Gómez Escalante <samuel.gomez.contractor@bbva.com>
2017-05-13 11:50:27 +0200

    stage deprecated

--- parallel.groovy
+++ parallel.groovy
@@ -1,7 +1,8 @@
 node {
   for (int i=0; i< 2; ++i) {  
-    stage "Stage #"+i
-    print 'Hello, world $i!'
+    stage("Stage #" + i){
+      print 'Hello, world $i!'
+    }
   }
 
   stage("Stage Parallel"){
